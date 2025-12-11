fabee9cd0faa264ccd27c4c5b337ee7774bfb4b2
Abduqodir <abdurahimovabduqdir96@gmail.com>
2025-12-11 17:05:33 +0500

    furst_commit

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from typing import Optional
 import json
 
 app = FastAPI()
 
+class ProductParial(BaseModel):
+    name: Optional[str] = None
+    price: Optional[int] = None
+
 class Product(BaseModel):
-    name:str
+    name: str
     price: float
     
 @app.get('/all_products')
@@ -45,7 +50,7 @@ def create_product(product: Product):
 @app.put("/update_product/{product_id}")
 def update_product(product_id: int, product:Product):
     try:
-        with open("products.json") as f:
+        with open("products.json", 'r') as f:
             data = json.load(f)
     except:
         return "product not found"
@@ -59,4 +64,43 @@ def update_product(product_id: int, product:Product):
                 json.dump(data, f, indent=4)
             return {"update_product": i}
         
+    return {"error": "product not found"}
+
+
+@app.patch("/update_product_partial/{product_id}")
+def partial_update_product(product_id: int, product:ProductParial):
+    try:
+        with open('products.json', 'r') as f:
+            data = json.load(f)
+    except:
+        return {"error": "product empty"}
+    
+    for i in data:
+        if i["id"] == product_id:
+            if product.name is not None:
+                i["name"] = product.name
+            if product.price is not None:
+                i['price'] = product.price
+            
+            with open("products.json", 'w') as f:
+                json.dump(data, f, indent=4)
+            return {"SUCCESS update_product": i}
+            
+    return {"error": "product not found"}
+
+
+@app.delete("/delete_product/{product_id}")
+def delete_product(product_id: int):
+    try:
+        with open("products.json", 'r') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        return {"error": "file not found"}
+    
+    for i, p in enumerate(data):
+        if p["id"] == product_id:
+            delete_p = data.pop(i)
+            with open("products.json", 'w') as f:
+                json.dump(data, f, indent=4)
+            return {"SUCCESS": f'object deleted {delete_p}'}
     return {"error": "product not found"}
